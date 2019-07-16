f89e81d6746032d765475fbce3b8ba5ea89f90c1
zhoulubo <zhoulubo@honhesns.com>
2019-07-16 16:06:49 +0800

    wert

---
+++ Project1/c_02.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+
+void mian(){
+
+	printf("say hello");
+
+
+}
+
+
+void class1(){ 
+
+
+}
